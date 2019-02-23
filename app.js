767732df7aa15f1123a823703dc1ad5f1d81ec87
Jonathan Cohen <jonathancohen@Jonathans-MacBook.local>
2019-02-23 16:34:13 -0500

    Added body parser

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const userRoute = require('./src/app/routes/user');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+mongoose.connect('mongodb://localhost/socialTorah');
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connection to Mongo successful');
+});
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
@@ -14,6 +25,7 @@ app.use(function(req, res, next) {
   next();
 });
 
+app.use(bodyParser.json());
 app.use('/user', userRoute);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
