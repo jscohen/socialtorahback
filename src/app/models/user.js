03c625f75866ea00f497840319d4c74b764d7783
Jonathan Cohen <jonathancohen@Jonathans-MacBook.local>
2019-02-23 16:34:44 -0500

    Commented out dummy code

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -1,6 +1,6 @@
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
+// const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -15,58 +15,58 @@ const userSchema = new mongoose.Schema({
   }
 });
 
-userSchema.plugin(uniqueValidator);
-
-userSchema.methods.comparePassword = function (password) {
-  const _this = this;
-
-  return new Promise((resolve, reject) =>
-    bcrypt.compare(password, _this.passwordDigest, (err, data) =>
-        err ? reject(err) : data ? resolve(data) : reject(new Error('Not Authorized')))
-    ).then(() => _this);
-};
-
-userSchema.virtual('password').set(function (password) {
-  this._password = password;
-});
-
-userSchema.pre('save', function (next) {
-  const _this = this;
-
-  if (!_this._password) {
-    return next();
-  }
-
-  new Promise((resolve, reject) =>
-    bcrypt.genSalt(null, (err, salt) =>
-        err ? reject(err) : resolve(salt))
-  ).then((salt) =>
-    new Promise((resolve, reject) =>
-      bcrypt.hash(_this._password, salt, (err, data) =>
-        err ? reject(err) : resolve(data)))
-  ).then((digest) => {
-    _this.passwordDigest = digest;
-    next();
-  }).catch((error) => {
-    next(error);
-  });
-});
-
-userSchema.methods.setPassword = function (password) {
-  const _this = this;
-
-  return new Promise((resolve, reject) =>
-    bcrypt.genSalt(null, (err, salt) =>
-        err ? reject(err) : resolve(salt))
-  ).then((salt) =>
-    new Promise((resolve, reject) =>
-      bcrypt.hash(password, salt, (err, data) =>
-        err ? reject(err) : resolve(data)))
-  ).then((digest) => {
-    _this.passwordDigest = digest;
-    return _this.save();
-  });
-};
+// userSchema.plugin(uniqueValidator);
+//
+// userSchema.methods.comparePassword = function (password) {
+//   const _this = this;
+//
+//   return new Promise((resolve, reject) =>
+//     bcrypt.compare(password, _this.passwordDigest, (err, data) =>
+//         err ? reject(err) : data ? resolve(data) : reject(new Error('Not Authorized')))
+//     ).then(() => _this);
+// };
+//
+// userSchema.virtual('password').set(function (password) {
+//   this._password = password;
+// });
+//
+// userSchema.pre('save', function (next) {
+//   const _this = this;
+//
+//   if (!_this._password) {
+//     return next();
+//   }
+//
+//   new Promise((resolve, reject) =>
+//     bcrypt.genSalt(null, (err, salt) =>
+//         err ? reject(err) : resolve(salt))
+//   ).then((salt) =>
+//     new Promise((resolve, reject) =>
+//       bcrypt.hash(_this._password, salt, (err, data) =>
+//         err ? reject(err) : resolve(data)))
+//   ).then((digest) => {
+//     _this.passwordDigest = digest;
+//     next();
+//   }).catch((error) => {
+//     next(error);
+//   });
+// });
+//
+// userSchema.methods.setPassword = function (password) {
+//   const _this = this;
+//
+//   return new Promise((resolve, reject) =>
+//     bcrypt.genSalt(null, (err, salt) =>
+//         err ? reject(err) : resolve(salt))
+//   ).then((salt) =>
+//     new Promise((resolve, reject) =>
+//       bcrypt.hash(password, salt, (err, data) =>
+//         err ? reject(err) : resolve(data)))
+//   ).then((digest) => {
+//     _this.passwordDigest = digest;
+//     return _this.save();
+//   });
+// };
 
 const User = mongoose.model('User', userSchema);
 
