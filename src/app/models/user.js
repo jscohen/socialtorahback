2163c74289fb3384828b707b7a170dc99abbcf62
Jonathan Cohen <jonathancohen@Jonathans-MacBook.local>
2019-02-25 13:27:20 -0500

    Added token, fixed logic

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -12,6 +12,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     unique: true,
     required: true
+  },
+  token: {
+    type: String,
+    required: true
   }
 });
 
--- src/app/routes/user.js
+++ src/app/routes/user.js
@@ -3,6 +3,14 @@ const app = express();
 const mongoose = require('mongoose');
 const router = express.Router();
 const User = require('../models/user');
+const crypto = require('crypto');
+
+const getToken = () =>
+  new Promise((resolve, reject) =>
+    crypto.randomBytes(16, (err, data) =>
+      err ? reject(err) : resolve(data.toString('base64'))
+    )
+  )
 
 router.get('/test', (req, res) => {
   console.log('On Test');
@@ -10,12 +18,21 @@ router.get('/test', (req, res) => {
 
 router.post('/signUp', (req, res) => {
   const userToSave = req.body;
-  User.findOne({email: req.body.email}).then((user) => {
-    if (user === null) {
-      new User(userToSave).save();
-    }
-    user ? res.send('User Already exists') : res.send(userToSave)
-  })
+  let userToSend = {};
+  getToken()
+      .then((token) => {
+        userToSave.token = token
+      })
+      .then(() => {
+        User.findOne({email: req.body.email}).then((user) => {
+          if (user === null) {
+            new User(userToSave).save();
+          }
+          userToSend = {'email': userToSave.email, 'token': userToSave.token}
+        user ? res.send('User Already exists') : res.send(userToSend)
+        })
+            .catch((err) => res.send('500'))
+      })
       .catch((err) => res.send('500'));
 });
 
