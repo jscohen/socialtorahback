8b5cae6ba2619e26e695add321e759a44396521c
Jonathan Cohen <jonathancohen@Jonathans-MacBook.local>
2019-03-08 20:00:53 -0500

    Refactor: adding pw encryption to user schema

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
+const SALT_WORK_FACTOR = 10;
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -19,38 +19,34 @@ const userSchema = new mongoose.Schema({
   }
 });
 
-// userSchema.plugin(uniqueValidator);
-//
-userSchema.methods.comparePassword = function(password, hash) {
-  return bcrypt.compareSync(password, hash);
+userSchema.pre(save, function(next) {
+  const user = this;
+
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next();
+
+  // generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if (err) return next(err);
+
+    // hash the password using our new salt
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) return next(err);
+
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
 };
-//
-// userSchema.virtual('password').set(function(password) {
-//   this._password = password;
-// });
-//
-// userSchema.pre('save', function(next) {
-//   const _this = this;
-//
-//   if (!_this._password) {
-//     return next();
-//   }
-//
-//   new Promise((resolve, reject) =>
-//     bcrypt.genSalt(null, (err, salt) =>
-//         err ? reject(err) : resolve(salt))
-//   ).then((salt) =>
-//     new Promise((resolve, reject) =>
-//       bcrypt.hash(_this._password, salt, (err, data) =>
-//         err ? reject(err) : resolve(data)))
-//   ).then((digest) => {
-//     _this.passwordDigest = digest;
-//     next();
-//   }).catch((error) => {
-//     next(error);
-//   });
-// });
-//
+
 userSchema.methods.setPassword = function(password) {
   const _this = this;
 
